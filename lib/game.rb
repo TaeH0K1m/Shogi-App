3d679238dd9ab1657f53e72afd923c0045f89ef3
johnung <johnung07@gmail.com>
2020-08-11 22:36:14 -0700

    Added valid move? Method for Pawn - Added helper methods

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,18 @@ module GameHelper
         row = square.to_s[1]
         (9 - col.to_i) + (row.to_i - 1)*9
     end
+
+    def get_pos_change(origin: , dest: )
+        origin_arr = [origin.to_s[0].to_i, origin.to_s[1].to_i]
+        dest_arr = [dest.to_s[0].to_i, dest.to_s[1].to_i]
+        dx = dest_arr[0] - origin_arr[0]
+        dy = dest_arr[1] - origin_arr[1]
+        [dx, dy]
+    end
+
+    def get_player
+        self[0] == '+' ? 'black' : 'white'
+    end
 end
 
 class Game
@@ -31,5 +43,38 @@ class Game
     def check_position(origin:, piece:, player:)
         turn = player == 'black' ? '+' : '-'
         board[to_idx(square: origin)] == "#{turn}#{piece}"
+    end
+
+    def move_piece(origin: , piece: , player: , dest: )
+
+        @board[to_idx(square: origin)] = '   '
+
+        turn = player == 'black' ? '+' : '-'
+        @board[to_idx(square: dest)] = "#{turn}#{piece}"
+    end
+
+    def valid_move?(origin: , piece: , player: , dest: )
+        # check if piece can make that move
+        # check if path is clear
+        case piece
+        when 'FU'
+            correct_ans = player == 'black' ? [0, -1] : [0, 1]
+
+            correct_move = correct_ans == get_pos_change(origin: origin, dest: dest) # valid_moves returns valid moves of pawn
+            correct_move && path_clear(origin: origin, piece: piece, player: player, dest: dest)
+        when 'KA'
+
+        else
+        end
+    end
+
+    def path_clear(origin: , piece: , player: , dest: )
+        case piece
+        when 'FU'
+            piece_on_dest = @board[to_idx(square: dest)]
+            piece_on_dest == '   ' || player != piece_on_dest.get_player
+        when 'KA'
+        else
+        end
     end
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -2,10 +2,10 @@ require 'game'
 
 include GameHelper
 
-describe "Game" do 
-    context "new" do 
+describe "Game" do
+    context "new" do
 
-       it "initializes the empty board" do 
+       it "initializes the empty board" do
             game = Game.new
             board = game.board
             expect(board).to eq %w[-]*80
@@ -26,9 +26,9 @@ describe "Game" do
                 '+KY', '+KE', '+GI', '+KI', '+OU', '+KI', '+GI', '+KE', '+KY'
             ]
             expect(board).to eq expected_board
-            
+
        end
-       
+
        it "is black's turn (first move)" do
             game = Game.new
             board = game.setup()
@@ -36,7 +36,7 @@ describe "Game" do
             expect(game.turn).to eq 'black'
        end
 
-       it "checks if the pieces are at the origin 1" do 
+       it "checks if the pieces are at the origin 1" do
             game = Game.new
             board = game.setup()
             valid_position = game.check_position(origin: 27, piece: 'FU', player: 'black')
@@ -44,7 +44,7 @@ describe "Game" do
             expect(valid_position).to eq true
        end
 
-       it "checks if the pieces are at the origin 2" do 
+       it "checks if the pieces are at the origin 2" do
             game = Game.new
             board = game.setup()
             valid_position = game.check_position(origin: 71, piece: 'GI', player: 'white')
@@ -52,39 +52,77 @@ describe "Game" do
             expect(valid_position).to eq true
         end
 
-        it "checks if the pieces are at the origin 3" do 
+        it "checks if the pieces are at the origin 3" do
             game = Game.new
             board = game.setup()
-            valid_position = game.check_position(origin: 59, piece: 'OU', player: 'black')
+            valid_position = game.check_position(origin: 59, piece: 'OU', player: 'black') # renane check_position -> valid_position?
 
             expect(valid_position).to eq true
         end
 
+        it 'moves piece' do
+          game = Game.new
+          board = game.setup()
+          game.move_piece(origin: 29, piece: 'KE', player: 'black', dest: 26)
+          hardcopy_board = [
+            '-KY', '-KE', '-GI', '-KI', '-OU', '-KI', '-GI', '-KE', '-KY',
+            '   ', '-HI', '   ', '   ', '   ', '   ', '   ', '-KA', '   ',
+            '-FU', '-FU', '-FU', '-FU', '-FU', '-FU', '-FU', '-FU', '-FU',
+            '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ',
+            '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ', '   ',
+            '   ', '   ', '   ', '   ', '   ', '   ', '   ', '+KE', '   ',
+            '+FU', '+FU', '+FU', '+FU', '+FU', '+FU', '+FU', '+FU', '+FU',
+            '   ', '+KA', '   ', '   ', '   ', '   ', '   ', '+HI', '   ',
+            '+KY', '+KE', '+GI', '+KI', '+OU', '+KI', '+GI', '   ', '+KY'
+        ]
+          expect(board).to eq hardcopy_board
+        end
+
+        it 'does it move' do
+          game = Game.new
+          board = game.setup()
+          valid_move = game.valid_move?(origin: 27, piece: 'FU', player: 'black', dest: 26)
+
+          expect(valid_move).to eq true
+        end
+
+        it 'does it move' do
+          game = Game.new
+          board = game.setup()
+          game.move_piece(origin: 29, piece: 'KE', player: 'black', dest: 26)
+          valid_move = game.valid_move?(origin: 27, piece: 'FU', player: 'black', dest: 26)
+
+          expect(valid_move).to eq false
+        end
+
+        it 'gets player' do
+         expect('+KI'.get_player).to eq 'black'
+        end
     #    it "test 2" do
     #         game = Game.new
     #         game.move_piece(square: '99', piece: 'FU')
     #         expect(game.is_occupy(square: '99', piece: 'FU')).to eq true
     #    end
-       
+
     #    it "test 3" do
     #         game = Game.new
     #         expect(Game.to_idx(square: 91)).to eq 0
-    #    end 
+    #    end
 
     #    it "test 4" do
     #         game = Game.new
     #         expect(Game.to_idx(square: 76)).to eq 47
-    #    end 
+    #    end
 
     #     it "test 5" do
     #         game = Game.new
     #         expect(Game.to_idx(square: 24)).to eq 34
-    #     end 
-    
+    #     end
+
     #     it "test 6" do
     #         game = Game.new
     #         expect(game.move_piece(square: 91, piece: 'FU')).to eq true
-    #     end 
-       
+    #     end
+
     end
  end
